99e716c939af176b83da8ace1e40a008ba1937a0
5appdata5 <148453509+5appdata5@users.noreply.github.com>
2025-01-07 14:03:35 +0000

    Add files via upload

--- mangaRename.py
+++ mangaRename.py
@@ -1,7 +1,7 @@
 import os
 
 filepath = "."
-manga_name = print(os.path.basename(os.path.abspath(filepath)))
+manga_name = os.path.basename(os.path.abspath(filepath))
 
 for file in os.listdir(filepath):
     oldname = os.path.join(filepath,file)
